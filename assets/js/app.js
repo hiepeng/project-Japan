35e55d95331fddd41f6d30fdac75110a97712a92
hiep <hiep>
2022-10-19 20:55:38 +0700

    update code

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,9 +21,8 @@ const imageClick = document.querySelectorAll(".image-posts");
 const buttonQuery = document.querySelectorAll(".button-slider button");
 
 function activeSlider() {
-  if(width >= 1024) {
-  console.log(slider);
-  slider[0].scroll(156, 0);
+  if(width > 1440) {
+    slider[0].scroll(698, 0);
   imageClick.forEach((btn) => {
     btn.addEventListener("click", () => {
       buttonQuery.forEach(button => {
@@ -34,16 +33,45 @@ function activeSlider() {
         buttonQuery[0].classList.add("activeButton");
       } else if (btn.classList.contains("itemSecond")) {
         buttonQuery[1].classList.add("activeButton");
-        slider[0].scroll({ top: 0, left: 156, behavior: "smooth" });
+        slider[0].scroll({ top: 0, left: 698, behavior: "smooth" });
       } else if (btn.classList.contains("itemThird")) {
         buttonQuery[2].classList.add("activeButton");
-        slider[0].scroll({ top: 0, left: 725, behavior: "smooth" });
+        slider[0].scroll({ top: 0, left: 1200, behavior: "smooth" });
       } else if (btn.classList.contains("itemFourth")) {
         buttonQuery[3].classList.add("activeButton");
-        slider[0].scroll({ top: 0, left: 1300, behavior: "smooth" });
+        slider[0].scroll({ top: 0, left: 1650, behavior: "smooth" });
       } else {
         buttonQuery[4].classList.add("activeButton");
-        slider[0].scroll({ top: 0, left: 2780, behavior: "smooth" });
+        slider[0].scroll({ top: 0, left: 10000, behavior: "smooth" });
+      }
+      imageClick.forEach((item) => item.classList.remove("active-item"));
+      if (!btn.classList.contains("active-item")) {
+        btn.classList.add("active-item");
+      }
+    });
+  });
+} else if(width > 1024){
+  slider[0].scroll(698, 0);
+  imageClick.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      buttonQuery.forEach(button => {
+        button.classList.remove('activeButton')
+      })
+      if (btn.classList.contains("itemFirst")) {
+        slider[0].scroll({ top: 0, left: 300, behavior: "smooth" });
+        buttonQuery[0].classList.add("activeButton");
+      } else if (btn.classList.contains("itemSecond")) {
+        buttonQuery[1].classList.add("activeButton");
+        slider[0].scroll({ top: 0, left: 698, behavior: "smooth" });
+      } else if (btn.classList.contains("itemThird")) {
+        buttonQuery[2].classList.add("activeButton");
+        slider[0].scroll({ top: 0, left: 1300, behavior: "smooth" });
+      } else if (btn.classList.contains("itemFourth")) {
+        buttonQuery[3].classList.add("activeButton");
+        slider[0].scroll({ top: 0, left: 1800, behavior: "smooth" });
+      } else {
+        buttonQuery[4].classList.add("activeButton");
+        slider[0].scroll({ top: 0, left: 2200, behavior: "smooth" });
       }
       imageClick.forEach((item) => item.classList.remove("active-item"));
       if (!btn.classList.contains("active-item")) {
@@ -52,6 +80,7 @@ function activeSlider() {
     });
   });
 } else{
+  slider[0].scroll(208, 0);
   imageClick.forEach(itemFor => 
     itemFor .classList.add("active-item")
   )
